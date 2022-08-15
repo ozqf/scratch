23ea850e8a5800dd35548aaa9bd599ef60f7ff7e
zqf <zqf@zealousquakefan.com>
2022-08-15 19:17:20 +0100

    Adding a crafting calculator and listing mats in obj files

--- node/crafting_calculator.js
+++ node/crafting_calculator.js
@@ -13,6 +13,10 @@ function findComponent(componentName, components) {
     return components[components.length - 1];
 }
 
+function findSimilarNames(components, searchString) {
+	return components.filter(c => c.name.includes(searchString)).map(c => c.name);
+}
+
 function addToResult(results, nameToAdd, countToAdd) {
     let result = results.find(r => r.name === nameToAdd);
     if (result) {
@@ -23,6 +27,16 @@ function addToResult(results, nameToAdd, countToAdd) {
         name: nameToAdd,
         count: countToAdd
     });
+}
+
+function comp(name, count) {
+	if (!count) {
+		count = 1;
+	}
+	return {
+		name: name,
+		count: count
+	};
 }
 
 function tallyIngredients(componentName, print, components, results) {
@@ -83,6 +97,20 @@ g_components = [
             { name: "computer chip" }
         ]
     },
+	{
+		name: "battery",
+		components: [
+			{ name: "acid mushroom", count: 2 },
+			{ name: "copper ore" }
+		]
+	},
+	{
+		name: "power cell",
+		components: [
+			{ name: "battery", count: 2 },
+			{ name: "silicone rubber" }
+		]
+	},
     {
         name: "power cell charger",
         components: [
@@ -142,7 +170,7 @@ g_components = [
         ]
     },
     {
-        name: "polyailine",
+        name: "polyaniline",
         components: [
             { name: "gold ore" },
             { name: "hydrochloric acid" }
@@ -151,6 +179,84 @@ g_components = [
     { name: "lubricant", components: [{ name: "creepvine seed cluster" }] },
     { name: "benzene", components: [{ name: "blood oil", count: 3 }] },
     { name: "polyaniline" },
+	{
+		name: "water filtration machine",
+		components: [
+			{ name: "titanium ore", count: 3 },
+			{ name: "copper wire", count: 1 },
+			{ name: "aerogel", count: 1 }
+		]
+	},
+	{
+		name: "thermal plant",
+		components: [
+			{ name: "titanium", count: 5 },
+			{ name: "magnetite", count: 2 },
+			{ name: "aerogel" },
+		]
+	},
+	{
+		name: "scanner room",
+		components: [
+			comp("titanium", 5),
+			comp("copper ore", 2),
+			comp("gold ore"),
+			comp("table coral sample")
+		]
+	},
+	{
+		name: "neptune gantry",
+		components: [
+			comp("plasteel ingot"),
+			comp("copper wire"),
+			comp("lubricant")
+		]
+	},
+	{
+		name: "neptune boosters",
+		components: [
+			comp("plasteel ingot"),
+			comp("nickel ore", 3),
+			comp("aerogel", 2),
+			comp("wiring kit")
+		]
+	},
+	{
+		name: "neptune fuel reserve",
+		components: [
+			comp("plasteel ingot"),
+			comp("crystalline sulfur", 4),
+			comp("kyanite", 4),
+			comp("ion power cell", 2)
+		]
+	},
+	{
+		name: "neptune cockpit",
+		components: [
+			comp("plasteel ingot"),
+			comp("cyclops shield generator"),
+			comp("enameled glass"),
+			comp("computer chip")
+		]
+	},
+	{
+		name: "modification station",
+		components: [ comp("computer chip"), comp("titanium"), comp("diamond"), comp("lead ore") ]
+	},
+	{
+		name: "power transmitter",
+		components: [ comp("gold"), comp("titanium") ]
+	},
+	{
+		name: "hatching enzymes",
+		components: [
+			comp("eye stalk seed"),	
+			comp("sea crown seed"),
+			comp("fungal sample"),
+			comp("ghost weed seed"),
+			comp("bulb bush sample")
+		]
+	},
     {
         name: "cyclops engine efficiency module",
         components: [
@@ -160,10 +266,26 @@ g_components = [
         ]
     },
     {
-        name: "cyclops depth module",
+        name: "cyclops depth module mk1",
         components: [
             { name: "plasteel ingot" },
             { name: "ruby", count: 3 }
+        ]
+    },
+	{
+        name: "cyclops depth module mk2",
+        components: [
+			comp("cyclops depth mk1"),
+			comp("plasteel ingot"),
+			comp("nickel ore", 3)
+        ]
+    },
+	{
+        name: "cyclops depth module mk3",
+        components: [
+			comp("cyclops depth mk2"),
+			comp("plasteel ingot"),
+			comp("kyanite", 3)
         ]
     },
     {
@@ -205,12 +327,46 @@ g_components = [
             { name: "lithium ore" }
         ]
     },
+	{
+		name: "prawn suit depth module mk1",
+		components: [
+			comp("plasteel ingot", 1),
+			comp("nickel ore", 3),
+			comp("ruby", 2)
+		]
+	},
+	{
+		name: "prawn suit propulsion cannon",
+		components: [
+			{ name: "computer chip" },
+			{ name: "titanium", count: 5 },
+			comp("magnetite", 2),
+			comp("lithium", 1)
+		]
+	},
+	{
+		name: "prawn suit jump jet upgrade",
+		components: [
+			{ name: "nickel ore", count: 2 },
+			{ name: "crystalline sulfur", count: 3 },
+			{ name: "titanium", count: 5 },
+			comp("lithium", 1)
+		]
+	},
     {
         name: "prawn drill arm",
         components: [
             { name: "titanium ore", count: 5 },
             { name: "lithium ore", count: 1 },
             { name: "diamond", count: 4 }
+        ]
+    },
+	{
+        name: "prawn drill torpedo arm",
+        components: [
+            { name: "titanium ore", count: 5 },
+            { name: "lithium ore", count: 1 },
+            { name: "aerogel", count: 1 }
         ]
     },
     {
@@ -235,5 +391,17 @@ if (process.argv.length > 2) {
 }
 console.log(`Tallying for ${desired}`);
 let results = [];
-tallyIngredients(desired, true, g_components, results);
-console.log(results.map(r => `${r.name} x${r.count}`));
+
+const requestedItems = findSimilarNames(g_components, desired);
+if (requestedItems.length === 0) {
+	console.log(`No compoents matching ${desired} found`);
+	return
+}
+requestedItems.forEach(item => {
+	console.log(`-----------------------------------------`);
+	tallyIngredients(item, true, g_components, results)
+	console.log(results.map(r => `${r.name} x${r.count}`));
+	results = [];
+});
+// tallyIngredients(desired, true, g_components, results);
+
